3d9f7ed3317a56c2eada722e2d63716b38ad09b6
VIETTHANG <vietphamthang2001@gmail.com>
2022-12-06 21:27:09 +0700

    Fixed D cate, Change frontend list cate,pub

--- src/main/java/tn412/project/controllers/CategoryController.java
+++ src/main/java/tn412/project/controllers/CategoryController.java
@@ -1,6 +1,5 @@
 package tn412.project.controllers;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,67 +16,63 @@ import tn412.project.repositories.CategoryRepository;
 @RequestMapping("/category")
 public class CategoryController {
 	@Autowired
-	private CategoryRepository theloaisachRepository;
+	private CategoryRepository categoryRepo;
+
 	@GetMapping("create")
-	 public String themtheloai(Model model) {
-       Category theloai = new Category();
-        model.addAttribute("category",theloai);
-        model.addAttribute("loi", new ResponseObject("",theloai));
-        
-        return "/category/them";
-    }
+	public String themtheloai(Model model) {
+		Category theloai = new Category();
+		model.addAttribute("category", theloai);
+		model.addAttribute("loi", new ResponseObject("", theloai));
+
+		return "/category/them";
+	}
+
 	@PostMapping("create")
-	public String themtheloais(Category theloai, Model model)
-	{
+	public String themtheloais(Category theloai, Model model) {
 		System.out.println(theloai.getName());
-		Category tls = theloaisachRepository.findByName(theloai.getName());
-		if (tls!=null)
-		{
+		Category tls = categoryRepo.findByName(theloai.getName());
+		if (tls != null) {
 			model.addAttribute("category", tls);
-			model.addAttribute("loi",new ResponseObject("Loi trung ten",tls));
+			model.addAttribute("loi", new ResponseObject("Loi trung ten", tls));
 			return "/category/them";
 		}
-		theloaisachRepository.save(theloai);
+		categoryRepo.save(theloai);
 		return "redirect:/category/create";
 	}
+
 	@GetMapping("edit/{id}")
-	public String suatls(Model model,@PathVariable Long id)
-	{
-		Category theloai = theloaisachRepository.findById(id).orElseThrow();
-		if(theloai!=null)
-		{
+	public String suatls(Model model, @PathVariable Long id) {
+		Category theloai = categoryRepo.findById(id).orElseThrow();
+		if (theloai != null) {
 			model.addAttribute("category", theloai);
-			model.addAttribute("loi", new ResponseObject("",theloai));
+			model.addAttribute("loi", new ResponseObject("", theloai));
 			return "/category/sua";
 		}
 		return "/category/danhsach";
 	}
+
 	@PostMapping("edit/{id}")
-	public String suatlss(Category theloai,@PathVariable Long id,Model model)
-	{
-		Category tls = theloaisachRepository.findById(id).orElseThrow();
-//		tls.setId(theloai.getId());
+	public String suatlss(Category theloai, @PathVariable Long id, Model model) {
+		Category tls = categoryRepo.findById(id).orElseThrow();
+		// tls.setId(theloai.getId());
 		tls.setName(theloai.getName());
-		theloaisachRepository.save(tls);
-		
+		categoryRepo.save(tls);
+
 		return "redirect:/category/danhsach";
 	}
+
 	@GetMapping("delete/{id}")
-	public String xoatls(@PathVariable Long id)
-	{
-		Category tls = theloaisachRepository.findById(id).orElseThrow();
-		theloaisachRepository.delete(tls);
-		
+	public String xoatls(@PathVariable Long id) {
+		Category deleteCategory = categoryRepo.findById(id).orElseThrow();
+		categoryRepo.delete(deleteCategory);
 		return "redirect:/category/danhsach";
-		
+
 	}
+
 	@GetMapping("danhsach")
-	public String danhsach(Model model)
-	{
-		model.addAttribute("listloaisach", theloaisachRepository.findAll());
+	public String danhsach(Model model) {
+		model.addAttribute("listloaisach", categoryRepo.findAll());
 		return "/category/danhsach";
 	}
-	
-	
 
 }
